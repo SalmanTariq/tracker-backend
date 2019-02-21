4baba513aa4743122f5ccda837fd736751ce81d3
Salman Tariq <salman.tariq90@gmail.com>
2019-02-21 16:49:55 +0500

    push

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+
+socket.on('connection', function (packet) {
+    console.log('a user connected');
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
